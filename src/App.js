dca5ec4562f889936d365966ec0f7e9d70f106d4
geekcoldhand <horatiousaharris@gmail.com>
2023-12-02 22:39:46 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import logo from "./logo.svg";
+import "./App.css";
+//import space from "./images/space";
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      
+      <li class="grid__item scroll-trigger animate--slide-in" data-cascade="" >
+                        
+
+      <div class="card-wrapper product-card-wrapper underline-links-hover">
+          <div class=" " >
+            <div class="card__inner color-background-2 gradient ratio" ><div class="card__content">
+                <div class="card__information">
+                  <h3 class="card__heading" id="title-template--21575234617646__product-grid-8926953013550">
+                    <a href="/products/tech-packs-fonts-textures" id="StandardCardNoMediaLink-template--21575234617646__product-grid-8926953013550" class="full-unstyled-link" aria-labelledby="StandardCardNoMediaLink-template--21575234617646__product-grid-8926953013550 NoMediaStandardBadge-template--21575234617646__product-grid-8926953013550">
+                      TECH PACKS FONTS &amp; TEXTURES
+                    </a>
+                  </h3>
+                </div>
+                <div class="card__badge bottom left"></div>
+              </div>
+            </div>
+            <div class="card__content">
+              <div class="card__information">
+                <h3 class="card__heading h5">
+                  <a href="/products/tech-packs-fonts-textures" id="CardLink-template--21575234617646__product-grid-8926953013550" class="full-unstyled-link" aria-labelledby="CardLink-template--21575234617646__product-grid-8926953013550 Badge-template--21575234617646__product-grid-8926953013550">
+                    TECH PACKS FONTS &amp; TEXTURES
+                  </a>
+                </h3>
+                <div class="card-information"><span class="caption-large light"></span>
+      <div class="
+          price ">
+        <div class="price__container"><div class="price__regular"><span class="visually-hidden visually-hidden--inline">Regular price</span>
+              <span class="price-item price-item--regular">
+                $100.00 USD
+              </span></div>
+          <div class="price__sale">
+              <span class="visually-hidden visually-hidden--inline">Regular price</span>
+              <span>
+                <s class="price-item price-item--regular"></s>
+              </span><span class="visually-hidden visually-hidden--inline">Sale price</span>
+            <span class="price-item price-item--sale price-item--last">
+              $100.00 USD
+            </span>
+          </div>
+
+          <small class="unit-price caption hidden">
+            <span class="visually-hidden">Unit price</span>
+            <span class="price-item price-item--last">
+              <span></span>
+              <span aria-hidden="true">/</span>
+              <span class="visually-hidden">&nbsp;per&nbsp;</span>
+              <span>
+              </span>
+            </span>
+          </small>
+        </div></div>
+
+      </div>
+              </div><div class="card__badge bottom left"></div>
+            </div>
+          </div>
+        </div>
+                      </li>
+     
+                        
+
+            <div class="card__inner color-background-2 gradient ratio" ><div class="card__content">
+                <div class="card__information">
+                  <h3 class="card__heading" id="title-template--21575234617646__product-grid-8927067144494">
+                    <a href="/products/copy-of-tech-packs-fonts-textures" id="StandardCardNoMediaLink-template--21575234617646__product-grid-8927067144494" class="full-unstyled-link" aria-labelledby="StandardCardNoMediaLink-template--21575234617646__product-grid-8927067144494 NoMediaStandardBadge-template--21575234617646__product-grid-8927067144494">
+                      TECH PACKS FONTS &amp; TEXTURES V2
+                    </a>
+                  </h3>
+                </div>
+              <div class="card__badge bottom left"></div>
+        </div>
+
+       </div>
+ 
+                <h3 class="card__heading h5">
+                  <a href="/products/copy-of-tech-packs-fonts-textures" id="CardLink-template--21575234617646__product-grid-8927067144494" class="full-unstyled-link" aria-labelledby="CardLink-template--21575234617646__product-grid-8927067144494 Badge-template--21575234617646__product-grid-8927067144494">
+                    TECH PACKS FONTS &amp; TEXTURES V2
+                  </a>
+                </h3>
+                <div class="card-information"><span class="caption-large light"></span>
+      <div class=" price ">
+        <div class="price__container"><div class="price__regular"><span class="visually-hidden visually-hidden--inline">Regular price</span>
+              <span class="price-item price-item--regular">
+                $300.00 USD
+              </span></div>
+          <div class="price__sale">
+              <span class="visually-hidden visually-hidden--inline">Regular price</span>
+            <span class="visually-hidden visually-hidden--inline">Sale price</span>
+            <span class="price-item price-item--sale price-item--last">
+              $300.00 USD
+            </span>
+          </div>
+          <small class="unit-price caption hidden">
+            <span class="visually-hidden">Unit price</span>
+            <span class="price-item price-item--last">
+              <span></span>
+              <span aria-hidden="true">/</span>
+              <span class="visually-hidden">&nbsp;per&nbsp;</span>
+              <span>
+              </span>
+            </span>
+          </small>
+        </div></div>
+    </div>    
+  </div> )}  
+  
+
+
+export default App;
