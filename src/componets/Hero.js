f7c7b7d1fe28d52b580c658144f0da2ee45876ba
geekcoldhand <horatiousaharris@gmail.com>
2024-01-06 13:49:47 -0500

    hero styling on text"

--- src/componets/Hero.js
+++ src/componets/Hero.js
@@ -1,22 +1,22 @@
 import heroImg from "../images/img5.png";
 export default function Hero() {
   return (
-    //make section blurry
 
-    <div className="hero mx-8 px-4">
+    <div className="hero mx-8 ">
       <div className="blur silhouette z-0  "></div>
-      <section className="absolute flex-col items-center h-full w-full z-10 px-7 hero-box lg:flex-row ">
-        <div className="hero-top-text w-3/5 flex-col justify-evenly ">
+      <section className="absolute flex-col justify-center items-center h-full w-full z-10 hero-box md:flex-row px-7">
+        <div className="hero-top-text w-5/5 flex-col justify-evenly sm:w-3/5">
        
-          <h3 className="subheading">Geek With A Cold Hand</h3>
-          <p className="my-3">
+          <h3 className="subheading"> 
+          <span className="hero-span pl-2 text-center"> Geek </span>With A Cold Hand</h3>
+          <p className="my-3  sm:block ">
           Molestias adipisicing elit. Molestias aut, repellat ipsum facere
           voluptate dicta obcaecati deserunt nobis suscipit eaque? aut, repellat
           ipsum facere voluptate
         </p>
         </div>
-
-        <div className="hero-bottom-text items-center w-2/5 my-8 lg:flex-col">
+        <hr></hr>
+        <div className="hero-bottom-text flex-col items-center w-2/5 my-8 ">
         <img className="hero-img my-4 " src={heroImg} alt="placeholder"></img>
         <button className =" call-to-action"> Learn More</button>
         </div>
